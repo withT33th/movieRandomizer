222dd461d13d04acb5cb33565cff5099bf1bfe16
withT33th <109703369+withT33th@users.noreply.github.com>
2025-03-13 00:02:31 -0400

    Adding the randomizing feature
    Added the randomizer feature to the project. Additionally, the csv file used to test the feature has also been added to the directory.

--- movieRandomizer.py
+++ movieRandomizer.py
@@ -1,12 +1,15 @@
 import csv
+import random
 
 listOfMovies = []
 
-with open("watchlist-witht33th-2024-12-31-02-25-utc.csv") as csv_file:
-    csvReader = csv.reader(csv_file, delimiter = ',')
+with open("watchlist-witht33th-2024-12-31-02-25-utc.csv", 'r') as csv_file:
+    csvReader = csv.DictReader(csv_file, delimiter = ',')   #using DictReader to clearly state what fieldname from the csv is being used
     next(csvReader) #skipping the header
 
+    #Adding the name of movie titles into an array
     for row in csvReader:
-        
+        listOfMovies.append(row['Name'])
 
-    
+    #Printing from the array the name of a randomly selected movie.
+    print(random.choice(listOfMovies))
