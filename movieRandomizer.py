dfd51d6eadd80b626a0d98fb8640341eeaf1547c
withT33th <109703369+withT33th@users.noreply.github.com>
2025-08-05 20:04:31 -0400

    Add 'main' method
    Added a main method as well as indented the existing code

--- movieRandomizer.py
+++ movieRandomizer.py
@@ -3,13 +3,19 @@ import random
 
 listOfMovies = []
 
-with open("watchlist-witht33th-2024-12-31-02-25-utc.csv", 'r') as csv_file:
-    csvReader = csv.DictReader(csv_file, delimiter = ',')   #using DictReader to clearly state what fieldname from the csv is being used
-    next(csvReader) #skipping the header
+# Adding a 'main' method
+def main():
 
-    #Adding the name of movie titles into an array
-    for row in csvReader:
-        listOfMovies.append(row['Name'])
+    with open("watchlist-witht33th-2024-12-31-02-25-utc.csv", 'r') as csv_file:
+        csvReader = csv.DictReader(csv_file, delimiter = ',')   #using DictReader to clearly state what fieldname from the csv is being used
+        next(csvReader) #skipping the header
 
-    #Printing from the array the name of a randomly selected movie.
-    print(random.choice(listOfMovies))
+        #Adding the name of movie titles into an array
+        for row in csvReader:
+            listOfMovies.append(row['Name'])
+
+        #Printing from the array the name of a randomly selected movie.
+        print(random.choice(listOfMovies))
+
+if __name__ == "__main__":
+    main()
