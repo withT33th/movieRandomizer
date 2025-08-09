ea576ba10888dfd8e5bf25921f0f6e9520f71627
withT33th <109703369+withT33th@users.noreply.github.com>
2025-08-08 23:32:16 -0400

    Create list generator method
    Moved the functionality of generating a list of movie title into a dedicated method

--- movieRandomizer.py
+++ movieRandomizer.py
@@ -1,15 +1,12 @@
 import csv
 import random
 import tkinter
+import pickle
 from tkinter.filedialog import askopenfilename
 
-# Adding a 'main' method
-def main():
+#defining a method to generate a list of movie titles from a csv file
+def generate_list(watchlist):
     listOfMovies = []
-
-    # Using the imported tkinter modules, we prompt the user to upload their movie watchlist from their file explorer
-    watchlist = askopenfilename()
-
     with open(watchlist, 'r') as csv_file:
         csvReader = csv.DictReader(csv_file, delimiter = ',')   #using DictReader to clearly state what fieldname from the csv is being used
         next(csvReader) #skipping the header
@@ -20,6 +17,15 @@ def main():
 
         #Printing from the array the name of a randomly selected movie.
         print(random.choice(listOfMovies))
+        return listOfMovies
+
+
+# Adding a 'main' method
+def main():
+    # Using the imported tkinter modules, we prompt the user to upload their movie watchlist from their file explorer
+    watchlist_file = askopenfilename()
+    watchlist = generate_list(watchlist_file)
+
 
 if __name__ == "__main__":
     main()
