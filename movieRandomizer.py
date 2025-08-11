d068952ca55983cd5ed48a92a415ce2fd9298da3
withT33th <109703369+withT33th@users.noreply.github.com>
2025-08-11 00:03:25 -0400

    Adding a randomize method
    Created a separate method that handles randomization of the list of movie titles instead of integrating that feature to the method generating said list

--- movieRandomizer.py
+++ movieRandomizer.py
@@ -5,9 +5,9 @@ import pickle
 from tkinter.filedialog import askopenfilename
 
 #defining a method to generate a list of movie titles from a csv file
-def generate_list(watchlist):
+def generate_list(watchlist_file):
     listOfMovies = []
-    with open(watchlist, 'r') as csv_file:
+    with open(watchlist_file, 'r') as csv_file:
         csvReader = csv.DictReader(csv_file, delimiter = ',')   #using DictReader to clearly state what fieldname from the csv is being used
         next(csvReader) #skipping the header
 
@@ -15,9 +15,11 @@ def generate_list(watchlist):
         for row in csvReader:
             listOfMovies.append(row['Name'])
 
-        #Printing from the array the name of a randomly selected movie.
-        print(random.choice(listOfMovies))
         return listOfMovies
+
+# returns a random element from the movielist
+def get_random_movie(movielist):
+    return random.choice(movielist)
 
 
 # Adding a 'main' method
@@ -25,6 +27,7 @@ def main():
     # Using the imported tkinter modules, we prompt the user to upload their movie watchlist from their file explorer
     watchlist_file = askopenfilename()
     watchlist = generate_list(watchlist_file)
+    print(get_random_movie(watchlist))
 
 
 if __name__ == "__main__":
